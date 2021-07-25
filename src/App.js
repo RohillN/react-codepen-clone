4ba2f4a979e11cdccbce1777249fda93636e6281
Rohill <rohillnand1@gmail.com>
2021-07-25 22:57:12 +1200

    basic codepen, three column editor, iframe serve data from editors

--- src/App.js
+++ src/App.js
@@ -1,10 +1,57 @@
-import logo from './logo.svg';
-// import './App.css';
+import React, { useState, useEffect } from 'react';
+import Editor from './components/Editor';
 
 function App() {
+
+  const [html, setHtml] = useState('');
+  const [css, setCss] = useState('');
+  const [javascript, setJavascript] = useState('');
+  const [srcDoc, setSrcDoc] = useState('');
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setSrcDoc(`
+        <html>
+          <body>${html}</body>
+          <style>${css}</style>
+          <script>${javascript}</script>
+        </html>
+      `)
+    }, 250)
+
+    return () => clearTimeout(timeout);
+  }, [html, css, javascript])
+
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <div className="pane top-pane">
+        <Editor
+          language="xml"
+          displayName="HTML"
+          value={html}
+          onChange={setHtml} />
+        <Editor
+          language="css"
+          displayName="CSS"
+          value={css}
+          onChange={setCss} />
+        <Editor
+          language="javascript"
+          displayName="JAVASCRIPT"
+          value={javascript}
+          onChange={setJavascript} />
+
+      </div>
+      <div className="pane">
+        <iframe
+          srcDoc={srcDoc}
+          title="output"
+          sandbox="allow-scripts"
+          frameBorder="0"
+          width="100%"
+          height="100%"
+        />
+      </div>
     </div>
   );
 }
---
+++ src/components/Editor.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/material.css';
+import 'codemirror/mode/xml/xml';
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/mode/css/css';
+import { Controlled as CodeMirror } from 'react-codemirror2'
+import * as AiIcons from 'react-icons/ai';
+
+export default function Editor(props) {
+    const {
+        language,
+        displayName,
+        value,
+        onChange,
+    } = props;
+
+    const [open, setOpen] = useState(true);
+
+    function handleChange(editor, data, value) {
+        onChange(value);
+    }
+
+    return (
+        <div className={`editor-container ${open ? "" : "collapsed"}`}>
+            <div className="editor-title">
+                {displayName}
+                <button 
+                type="button"
+                className="expand-collapsed-btn"
+                onClick={() => setOpen(prevOpen => !prevOpen)}>
+                    {open ? <AiIcons.AiOutlineShrink size={22} /> : <AiIcons.AiOutlineExpandAlt size={22} />}
+                </button>
+            </div>
+            <CodeMirror
+                value={value}
+                onBeforeChange={handleChange}
+                className="code-mirror-wrapper"
+                options={{
+                    lineWrapping: true,
+                    lint: true,
+                    mode: language,
+                    lineNumbers: true,
+                    theme: 'material'
+                }}
+            />
+        </div>
+    )
+}
